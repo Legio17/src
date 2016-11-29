d40c50168dafaf579f06f0877687da52217f5007
anaiuliachifor <anaiuliachifor@gmail.com>
2016-11-29 13:28:01 +0100

    stuff

--- database/methods/InsertExecutor.java
+++ database/methods/InsertExecutor.java
@@ -5,6 +5,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * @author Adam Szekely, Ana-Iulia Chifor, Filip Hudec, Peter Miodrag Varanic, Signe Rasmussen
+ *
+ */
 public class InsertExecutor {
 
 	
@@ -14,9 +18,20 @@ public class InsertExecutor {
 		this.connection = connection;	
 	}
 	
+	/**
+	 * A method that adds information about a finished game to the Game_History table, and then it will update the Score_Info table
+	 * with the new wins/losses values, win/loss ratio and rank for each player
+	 * @param player1 -adds the name of the 1st player to the Game_History table
+	 * @param player2 -adds the name of the 2nd player to the Game_History table
+	 * @param type -adds the type of the board (3X3 or 5X5) to the Game_History table
+	 * @param date -adds the date when the game was played to the Game_History table
+	 * @param result -adds the result of the 1st player (won/lost/tie) to the Game_History table
+	 * @throws SQLException
+	 */
+	
 	public void insertToGameHistory(String player1, String player2, String type,String date, String result) throws SQLException{
 		String sql;
-		String sqlUpdate;
+		String sqlUpdate="";
 		String sqlRetrivePlayers;
 		Statement st = connection.createStatement();
 		Statement stRetrive = connection.createStatement();
@@ -26,6 +41,7 @@ public class InsertExecutor {
 		sql = "INSERT INTO Game_History VALUES(";
         sql += "(SELECT COUNT(*) FROM Game_History)"+",'"+player1 +"','" + player2 + "','" + date + "','" + type + "','" + result +"');";
         
+        // in case player1 wins the game
 		if (result.equals("won")){
 			sqlRetrivePlayers = "SELECT login, wins, losses FROM score_info";
 			stRetrive = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -36,41 +52,51 @@ public class InsertExecutor {
 	        		int wins=Integer.parseInt(rsRetrive.getString("wins"))+1;
 	        		int losses= Integer.parseInt(rsRetrive.getString("losses"));
 	        		double ratio= (double) wins/losses;
-	        		
+	        		sqlUpdate="UPDATE Score_info SET wins="+wins+" WHERE  login='"+player1+"'";
+	        		sqlUpdate="UPDATE Score_info SET winloss_ratio="+ratio+" WHERE  login='"+player1+"'";
+	        	}
+	        	else if(rsRetrive.getString("login").equalsIgnoreCase("player2")){
+	        		int wins=Integer.parseInt(rsRetrive.getString("wins"));
+	        		int losses= Integer.parseInt(rsRetrive.getString("losses"))+1;
+	        		double ratio= (double) wins/losses;
+	        		sqlUpdate="UPDATE Score_info SET wins="+losses+" WHERE  login='"+player2+"'";
+	        		sqlUpdate="UPDATE Score_info SET winloss_ratio="+ratio+" WHERE  login='"+player2+"'";
 	        	}
 	        }
+	        sqlMet.executeStatement(st, sqlUpdate);
 		}
+		
+		// in case player2 wins
+		else if (result.equals("lost")){
+			sqlRetrivePlayers = "SELECT login, wins, losses FROM score_info";
+			stRetrive = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			ResultSet rsRetrive = stRetrive.executeQuery(sqlRetrivePlayers);
+			
+	        while(rsRetrive.next()){
+	        	if(rsRetrive.getString("login").equalsIgnoreCase("player1")){
+	        		int wins=Integer.parseInt(rsRetrive.getString("wins"));
+	        		int losses= Integer.parseInt(rsRetrive.getString("losses")+1);
+	        		double ratio= (double) wins/losses;
+	        		sqlUpdate="UPDATE Score_info SET wins="+losses+" WHERE  login='"+player1+"'";
+	        		sqlUpdate="UPDATE Score_info SET winloss_ratio="+ratio+" WHERE  login='"+player1+"'";
+	        	}
+	        	else if(rsRetrive.getString("login").equalsIgnoreCase("player2")){
+	        		int wins=Integer.parseInt(rsRetrive.getString("wins")+1);
+	        		int losses= Integer.parseInt(rsRetrive.getString("losses"));
+	        		double ratio= (double) wins/losses;
+	        		sqlUpdate="UPDATE Score_info SET wins="+wins+" WHERE  login='"+player2+"'";
+	        		sqlUpdate="UPDATE Score_info SET winloss_ratio="+ratio+" WHERE  login='"+player2+"'";
+	        	}
+	        }
+	        sqlMet.executeStatement(st, sqlUpdate);
+		}
+		
+		
 		sqlMet.executeStatement(st, sql);
 	} 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public void insertPlayer(String login) throws SQLException{
-		String sqlRetrivePlayers, model;
+		String sqlRetrivePlayers, sqlCreateScoreInfo, model;
 		Statement stWriteTo = connection.createStatement(), stRetrive = connection.createStatement();
 		
 		model = "applejack";
@@ -90,7 +116,9 @@ public class InsertExecutor {
         }
         String sqlAddLogin;
 		sqlAddLogin = "INSERT INTO PLAYER_INFO VALUES('"+login+"','"+model+"');";
+		sqlCreateScoreInfo = "INSERT INTO SCORE_INFO VALUES((SELECT COUNT(*) FROM SCORE_INFO),'"+login+"','0','0','0','0');";
 		
 		sqlMet.executeStatement(stWriteTo, sqlAddLogin);
+		sqlMet.executeStatement(stWriteTo, sqlCreateScoreInfo);
 	}
 }
