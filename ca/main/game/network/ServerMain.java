7d18e0306bc226932d0d504e2048e72705b9ef70
S. Rasmussen <siras@outlook.com>
2016-11-30 15:33:02 +0100

    ServerMain update

--- ca/main/game/network/ServerMain.java
+++ ca/main/game/network/ServerMain.java
@@ -7,9 +7,15 @@ import java.net.InetAddress;
 
 import java.net.SocketException;
 
-
-
-public class ServerMain extends Thread{
+/**
+ * A class representing a server that receives data packets from a client or
+ * send data packets to it
+ * 
+ * @author Filip Hudec, Signe Rasmussen, Peter Miodrag Veranic, Adam Szekely,
+ *         Ana Iulia Chifor
+ *
+ */
+public class ServerMain extends Thread {
 
 	private DatagramSocket serverSocket;
 	private byte[] receiveData, sendData;
@@ -19,28 +25,35 @@ public class ServerMain extends Thread{
 	private boolean newData;
 	private ConnectionList cl = new ConnectionList();
 
+	/**
+	 * No-arg constructor initializing the datagram socket for sending and
+	 * receiving datagram packets.
+	 */
 	public ServerMain() {
-
 		try {
 			serverSocket = new DatagramSocket(1099);
-			System.out.println("Server socket created.");
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * This is a method needed to start the thread. When the method start is
+	 * called on an object of this class it will execute the code in the run
+	 * method. -----------------------------
+	 */
 	public void run() {
-
 		while (true) {
 			newData = false;
 			receiveData = new byte[25];
-			
+
 			receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			
+
 			try {
 				serverSocket.receive(receivePacket);
 				newData = true;
-				//System.out.println("receiving data"+new String(receivePacket.getData()));
+				// System.out.println("receiving data"+new
+				// String(receivePacket.getData()));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -48,58 +61,49 @@ public class ServerMain extends Thread{
 			ipAddress = receivePacket.getAddress();
 			port = receivePacket.getPort();
 
-			createConn(sendData, ipAddress, port);	
-			
+			createConn(sendData, ipAddress, port);
+
 		}
 	}
-	
-	public InetAddress getIP()
-	{
-		return ipAddress;
-	}
-	
-	public byte[] getData()
-	{
+
+	public byte[] getData() {
 		return sendData;
 	}
-	
-	public int getPort()
-	{
+
+	public int getPort() {
 		return port;
 	}
-	
-	public DatagramSocket getServerSocket()
-	{
+
+	public DatagramSocket getServerSocket() {
 		return serverSocket;
 	}
-	
-	public ConnectionList getList()
-	{
+
+	public ConnectionList getList() {
 		return cl;
 	}
 
 	private void createConn(byte[] sendData, InetAddress ipAddress, int port) {
 
 		boolean found = false;
-		
+
 		for (int i = 0; i < cl.size(); i++) {
 			if (ipAddress.equals(cl.getIP(i))) {
 				found = true;
 				break;
 			}
 		}
-		
+
 		if (!found) {
 			Connection newCon = new Connection(sendData, ipAddress, port);
 			cl.addConnection(newCon);
-			
-			
+
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		ServerMain server = new ServerMain();
+		System.out.println("Server socket created.");
 		BroadcastToClients btc = new BroadcastToClients(server);
 		btc.start();
 		server.start();
