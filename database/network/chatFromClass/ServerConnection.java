adf035618ae2a8a4db55a7c37ba16e7997769955
anaiuliachifor <anaiuliachifor@gmail.com>
2016-11-30 12:04:23 +0100

    chat from class

---
+++ database/network/chatFromClass/ServerConnection.java
@@ -0,0 +1,55 @@
+package database.network.chatFromClass;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerConnection implements Runnable {
+
+	private Socket clientSocket;
+	private ObjectOutputStream outToClient;
+	private ObjectInputStream inFromClient;
+	private MessageBroadcast mb;
+
+	public ServerConnection(Socket connectionSocket) {
+		try{
+		clientSocket = connectionSocket;
+		outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
+		inFromClient = new ObjectInputStream(connectionSocket.getInputStream());}
+		catch(IOException e){}
+	}
+
+	public ServerConnection(Socket connectionSocket, MessageBroadcast mb) {
+		try{
+		clientSocket = connectionSocket;
+		this.mb=mb;
+		outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
+		inFromClient = new ObjectInputStream(connectionSocket.getInputStream());}
+		catch(IOException e){}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+
+				// read message from client.
+				Message message = (Message) inFromClient.readObject();
+				System.out.println("Message from Client: " + message);
+
+				// Send reply to client.
+				Message replyMessage = new Message(message.getId(), message
+						.getBody().toUpperCase());
+				for(int i=0; i<mb.getsize();i++){
+					mb.getConn(i).outToClient.writeObject(replyMessage);;
+				}
+				//System.out.println("Server reply: " + replyMessage);
+				//outToClient.writeObject(replyMessage);
+			} catch (IOException | ClassNotFoundException e) {
+			}
+		}
+	}
+
+}
