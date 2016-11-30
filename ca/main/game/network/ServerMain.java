9c468a927866c52a7bd1224c8e4e4246e3f67265
S. Rasmussen <siras@outlook.com>
2016-11-30 16:23:50 +0100

    ctrl Z ServerMain

--- ca/main/game/network/ServerMain.java
+++ ca/main/game/network/ServerMain.java
@@ -4,11 +4,18 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+
 import java.net.SocketException;
 
-
-
-public class ServerMain extends Thread{
+/**
+ * A class representing a server that receives data packets from a client or
+ * send data packets to it
+ * 
+ * @author Filip Hudec, Signe Rasmussen, Peter Miodrag Veranic, Adam Szekely,
+ *         Ana Iulia Chifor
+ *
+ */
+public class ServerMain extends Thread {
 
 	private DatagramSocket serverSocket;
 	private byte[] receiveData, sendData;
@@ -16,95 +23,123 @@ public class ServerMain extends Thread{
 	private InetAddress ipAddress;
 	private int port;
 	private boolean newData;
-	private ConnectionList cl = new ConnectionList();
+	private ConnectionList connections = new ConnectionList();
 
+	/**
+	 * No-arg constructor initializing the datagram socket for sending and
+	 * receiving datagram packets.
+	 */
 	public ServerMain() {
-
 		try {
 			serverSocket = new DatagramSocket(1099);
-			System.out.println("Server socket created.");
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * This is a method needed to start the thread. When the method start is
+	 * called on an object of this class it will execute the code in the run
+	 * method. -----------------------------
+	 */
 	public void run() {
-
 		while (true) {
 			newData = false;
 			receiveData = new byte[25];
-			
+
 			receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
 			try {
 				serverSocket.receive(receivePacket);
 				newData = true;
-				//System.out.println("receiving data"+new String(receivePacket.getData()));
+				// System.out.println("receiving data"+new
+				// String(receivePacket.getData()));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			sendData = receivePacket.getData();
-			
 			ipAddress = receivePacket.getAddress();
 			port = receivePacket.getPort();
 
-			createConn(sendData, ipAddress, port);	
-			
+			createConn(sendData, ipAddress, port);
+
 		}
 	}
-	
-	public InetAddress getIP()
-	{
-		return ipAddress;
-	}
-	
-	public byte[] getData()
-	{
+
+	/**
+	 * The server will receive data from a client. This data needs to be send to
+	 * all clients connected.
+	 * 
+	 * @return sendData the data that needs to be send to all clients
+	 */
+	public byte[] getData() {
 		return sendData;
 	}
-	
-	public int getPort()
-	{
-		return port;
-	}
-	
-	public DatagramSocket getServerSocket()
-	{
+
+	/**
+	 * @return serverSocket the datagram socket of the server
+	 */
+	public DatagramSocket getServerSocket() {
 		return serverSocket;
 	}
-	
-	public ConnectionList getList()
-	{
-		return cl;
+
+	/**
+	 * @return connections the list of connection objects
+	 */
+	public ConnectionList getConnectionList() {
+		return connections;
 	}
 
+	/**
+	 * Create a new connection if the ipAddress is not already in the list
+	 * 
+	 * @param sendData
+	 *            the data the client is sending to the server
+	 * @param ipAddress
+	 *            the ipAddress of the client communicating with the server
+	 * @param port
+	 *            the --------------------------------------
+	 */
 	private void createConn(byte[] sendData, InetAddress ipAddress, int port) {
 
 		boolean found = false;
-		
-		for (int i = 0; i < cl.size(); i++) {
-			if (ipAddress.equals(cl.getIP(i))) {
+
+		for (int i = 0; i < connections.size(); i++) {
+			if (ipAddress.equals(connections.getIP(i))) {
 				found = true;
 				break;
 			}
 		}
-		
+
 		if (!found) {
 			Connection newCon = new Connection(sendData, ipAddress, port);
-			cl.addConnection(newCon);
-			
-			
+			connections.addConnection(newCon);
+
 		}
-		
+
 	}
 
+	/**
+	 * Boolean to unsure that the BroadcastToClients only broadcasts when the
+	 * server has received new data to not broadcast redundantly.
+	 * 
+	 * @return newData a boolean that should be true each time a new packet is received and false the rest of the time
+	 */
+	public boolean isNewData() {
+		return newData;
+	}
+
+	/**
+	 * Main method runs the thread for receiving data and the thread for
+	 * broadcasting the received data
+	 * ------------------------------------------------------------
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ServerMain server = new ServerMain();
+		System.out.println("Server socket created.");
 		BroadcastToClients btc = new BroadcastToClients(server);
 		btc.start();
 		server.start();
-	}
-
-	public boolean isNewData() {
-		return newData;
 	}
 }
