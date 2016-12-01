a7856950e91709c70a5c31b453591dacac4ad242
S. Rasmussen <siras@outlook.com>
2016-12-01 14:28:31 +0100

    Sysout to remove later

--- ca/main/game/network/BroadcastToClients.java
+++ ca/main/game/network/BroadcastToClients.java
@@ -26,7 +26,8 @@ public class BroadcastToClients extends Thread {
 				for (int i = 0; i < cl.size(); i++) {
 					
 					if (server.getData() != null){
-						sendPacket = new DatagramPacket(server.getData(), server.getData().length, cl.getIP(i), cl.getPort(i));						
+						sendPacket = new DatagramPacket(server.getData(), server.getData().length, cl.getIP(i), cl.getPort(i));
+						System.out.println(UDPMethods.DatagramPacketToString(sendPacket));
 						try {
 							serverSocket.send(sendPacket);
 						} catch (IOException e) {
