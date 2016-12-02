8d4aa002f8b4f413f3d6c7daa3c86423d1c0a27c
adamszekely <szekelyadam93@gmail.com>
2016-12-02 14:50:55 +0100

    user name check

--- ca/main/game/network/TCPServer/ServerConnection.java
+++ ca/main/game/network/TCPServer/ServerConnection.java
@@ -13,7 +13,6 @@ import database.connection.connect;
 import database.methods.database_methods;
 import database.network.chatFromClass.Message;
 
-
 public class ServerConnection {
 
 	private Socket clientSocket;
@@ -22,71 +21,90 @@ public class ServerConnection {
 	private dbClientList<ServerConnection> dbClientList;
 	private Connection con = null;
 	private String name;
+	private String data;
+	String[] array;
 
-
-
-	public ServerConnection(Socket connectionSocket, dbClientList<ServerConnection> dbClientList) {
-		try{
-		con = connect.PostgreSQLJDBC("SEP2_data", "peter28mio07");
-		clientSocket = connectionSocket;
-		this.dbClientList = dbClientList;
-		outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
-		inFromClient = new ObjectInputStream(connectionSocket.getInputStream());}
-		catch(IOException e){} 
-		catch (ClassNotFoundException e) {
+	public ServerConnection(Socket connectionSocket,
+			dbClientList<ServerConnection> dbClientList) {
+		try {
+			con = connect.PostgreSQLJDBC("SEP2_data", "peter28mio07");
+			clientSocket = connectionSocket;
+			this.dbClientList = dbClientList;
+			outToClient = new ObjectOutputStream(
+					connectionSocket.getOutputStream());
+			inFromClient = new ObjectInputStream(
+					connectionSocket.getInputStream());
+		} catch (IOException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	
-	public String getPlayerName()
-	{
-	return name;
+	 public String getPlayerName() { return name; }
+	 
+	 public void setPlayerName(String name) {
+	
+	 this.name = name; }
+	 
+
+	public void check() {
+		try {
+			data = (String) inFromClient.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		if (data.contains(":")) {
+			array = data.split(":");
+			if (array[0].equals("00")) {
+				updateDB();
+			} else {
+				send();
+			}
+		}
 	}
-	
-	public void setPlayerName(String name)
-	{
-		
-			this.name = name;	
+
+	public void updateDB() {
+		System.out.println(array[1]);
 	}
 
 	public void send() {
-		//while (true) {
-			String name=null;
-			String info=null;
-			try {
-				
-				name = (String) inFromClient.readObject();
-				setPlayerName(name);
-			
-				info = database_methods.getInfoByName(con, name);
-				
-				//System.out.println(info);
-				
-				// Send reply to client.
-				
-				Iterator<ServerConnection> iterator = dbClientList.iterator();
-				//System.out.println(""+iterator.next());
-				while(iterator.hasNext())
-				{
-					System.out.println(dbServer.getC());
-					if(dbClientList.contains(dbServer.getC()))
-					{
-						System.out.println("here");
-						//System.out.println("Result: "+dbClientList.getCon(dbServer.getC()));
-						dbClientList.getCon(dbServer.getC()).outToClient.writeObject(info);
+		// while (true) {
+		//String name = null;
+		String info = null;
+		try {
+
+		//	 name = (String) inFromClient.readObject();
+			 setPlayerName(data);
+
+			info = database_methods.getInfoByName(con, data);
+
+			// System.out.println(info);
+
+			// Send reply to client.
+
+			Iterator<ServerConnection> iterator = dbClientList.iterator();
+			// System.out.println(""+iterator.next());
+			while (iterator.hasNext()) {
+				System.out.println(dbServer.getC());
+				if (dbClientList.contains(dbServer.getC())) {
+					System.out.println("here");
+					// System.out.println("Result: "+dbClientList.getCon(dbServer.getC()));
+					dbClientList.getCon(dbServer.getC()).outToClient
+							.writeObject(info);
 					break;
-					}
 				}
-				con.close();
-				//System.out.println("Server reply: " + replyMessage);
-				//outToClient.writeObject(replyMessage);
-			} catch (IOException | ClassNotFoundException | SQLException e) {
-				e.printStackTrace();
 			}
+			con.close();
+			// System.out.println("Server reply: " + replyMessage);
+			// outToClient.writeObject(replyMessage);
+		} catch (IOException | SQLException e) {
+			e.printStackTrace();
 		}
-//	}
+	}
+	// }
 
 }
